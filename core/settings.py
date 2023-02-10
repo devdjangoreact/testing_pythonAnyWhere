a572a872a5f7a67a2d5b50d6584e1f33fec637d1
devdjangoreact <devdjangoreact@gmail.com>
2023-02-10 14:46:30 +0000

    cors

--- core/settings.py
+++ core/settings.py
@@ -181,3 +181,5 @@ SWAGGER_SETTINGS = {
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CORS_ORIGIN_ALLOW_ALL = True
